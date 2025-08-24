fb27625cabc867788dc52de24ba6dead6874f5a1
kaio <kaio22barra@gmail.com>
2025-08-24 18:58:52 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        aluno aluno1 = new aluno();
+        aluno aluno2 = new aluno();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        aluno1.nome = "Gabriel Barbosa";
+        aluno1.matricula = 101;
+        aluno1.notaav1 = 5;
+        aluno1.notaav2 = 7;
+
+        aluno2.nome = "Giorgian De Arrascaeta";
+        aluno2.matricula = 102;
+        aluno2.notaav1 = 9;
+        aluno2.notaav2 = 9;
+
+        aluno1.dados();
+        aluno1.verificarAprovacao();
+
+        aluno2.dados();
+        aluno2.notaav1 = 10;
+        aluno2.verificarAprovacao();
     }
 }
