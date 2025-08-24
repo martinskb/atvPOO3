ae3b98d62aa0ac2c7c4386c05d151e5df3b892d4
kaio <kaio22barra@gmail.com>
2025-08-24 18:58:16 -0300

    Update aluno.java

--- src/aluno.java
+++ src/aluno.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class aluno {
+    String nome;
+    int matricula;
+    double notaav1;
+    double notaav2;
+    double media;
+
+    public void dados() {
+        System.out.println("Nome do aluno: " + nome + " | Matrícula: " + matricula);
+    }
+
+    public void verificarAprovacao() {
+        System.out.println("Notas: " + notaav1 + ", " + notaav2);
+        media = (notaav1 + notaav2) / 2;
+        System.out.println("Média: " + media);
+        if (media >= 7) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
 }
